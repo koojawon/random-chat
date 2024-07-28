153675b93d3aa3f6b17789ad122ccd6d27990e04
koojawon <jawonkoo@naver.com>
2024-07-28 20:42:05 +0900

    -refactor 이름 변경 및 레포지토리 통합

---
+++ src/main/java/com/rchat/randomChat/websocket/service/WebsocketManager.java
@@ -0,0 +1,31 @@
+package com.rchat.randomChat.websocket.service;
+
+import com.rchat.randomChat.websocket.repository.WebSocketSessionRepository;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+@Service
+@RequiredArgsConstructor
+public class WebsocketManager {
+    private final WebSocketSessionRepository sessionRepository;
+
+    public void sendMessage(String sessionId, TextMessage message) throws IOException {
+        WebSocketSession session = sessionRepository.get(sessionId);
+        session.sendMessage(message);
+    }
+
+    public void join(WebSocketSession session) {
+        sessionRepository.put(session);
+    }
+
+    public void leave(WebSocketSession session) {
+        sessionRepository.remove(session);
+    }
+
+    public boolean checkAliveById(String sessionId) {
+        return sessionRepository.get(sessionId).isOpen();
+    }
+}
