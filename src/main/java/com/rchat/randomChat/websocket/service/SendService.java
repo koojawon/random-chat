e4c708830a2e12754b7213f7adaee05902a7e3b0
koojawon <jawonkoo@naver.com>
2024-07-28 11:24:19 +0900

    -refactor 전송 방식 통합

--- src/main/java/com/rchat/randomChat/websocket/service/SendService.java
+++ src/main/java/com/rchat/randomChat/websocket/service/SendService.java
@@ -1,9 +1,6 @@
 package com.rchat.randomChat.websocket.service;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.rchat.randomChat.websocket.repository.WebSocketSessionRepository;
-import com.rchat.randomChat.websocket.statics.SdpGenerationOrderMessage;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,36 +11,10 @@ import org.springframework.web.socket.WebSocketSession;
 @RequiredArgsConstructor
 public class SendService {
 
-    private final WebSocketSessionRepository webSocketSessionRepository;
     private final Gson gson = new Gson();
 
-    public void sendSdp(String opponentId, JsonObject jsonObject) throws IOException {
-        WebSocketSession session = webSocketSessionRepository.get(opponentId);
+    public void sendMessage(WebSocketSession session, Object jsonObject) throws IOException {
         String message = gson.toJson(jsonObject);
         session.sendMessage(new TextMessage(message));
-    }
-
-    public void orderGenerateOffer(String id) throws IOException {
-        WebSocketSession session = webSocketSessionRepository.get(id);
-        String message = gson.toJson(new SdpGenerationOrderMessage());
-        session.sendMessage(new TextMessage(message));
-    }
-
-    public void sendIceCandidate(String id, JsonObject jsonObject) throws IOException {
-        WebSocketSession session = webSocketSessionRepository.get(id);
-        String message = gson.toJson(jsonObject);
-        session.sendMessage(new TextMessage(message));
-    }
-
-    public void sendStop(String opponentId, JsonObject jsonObject) throws IOException {
-        WebSocketSession session = webSocketSessionRepository.get(opponentId);
-        String message = gson.toJson(jsonObject);
-        session.sendMessage(new TextMessage(message));
-    }
-
-    public boolean checkAlive(String id) {
-        WebSocketSession session = webSocketSessionRepository.get(id);
-
-        return session.isOpen();
     }
 }
