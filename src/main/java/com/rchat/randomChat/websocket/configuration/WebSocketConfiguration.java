832ba8adc1952c6e7058ed2605955becb407fd75
koojawon <jawonkoo@naver.com>
2024-07-28 11:25:02 +0900

    -refactor 불필요한 어노테이션 제거

--- src/main/java/com/rchat/randomChat/websocket/configuration/WebSocketConfiguration.java
+++ src/main/java/com/rchat/randomChat/websocket/configuration/WebSocketConfiguration.java
@@ -2,13 +2,11 @@ package com.rchat.randomChat.websocket.configuration;
 
 import com.rchat.randomChat.websocket.handler.SimpleWebSocketHandler;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Slf4j
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
