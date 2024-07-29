1cf08f97374c03404e70c8825bbc9ec4a3b6e8fa
koojawon <jawonkoo@naver.com>
2024-07-29 14:30:54 +0900

    -fix 메시지 전송 방식 변경

--- src/main/java/com/rchat/randomChat/websocket/service/WebsocketManager.java
+++ src/main/java/com/rchat/randomChat/websocket/service/WebsocketManager.java
@@ -1,20 +1,24 @@
 package com.rchat.randomChat.websocket.service;
 
+import com.google.gson.Gson;
 import com.rchat.randomChat.websocket.repository.WebSocketSessionRepository;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WebsocketManager {
     private final WebSocketSessionRepository sessionRepository;
+    private final Gson gson = new Gson();
 
-    public void sendMessage(String sessionId, TextMessage message) throws IOException {
+    public void sendMessage(String sessionId, Object message) throws IOException {
         WebSocketSession session = sessionRepository.get(sessionId);
-        session.sendMessage(message);
+        session.sendMessage(new TextMessage(gson.toJson(message)));
     }
 
     public void join(WebSocketSession session) {
