4dc3ecf66899fbd6d91cb27e9e4949c0c70aa4b9
koojawon <jawonkoo@naver.com>
2024-08-12 16:39:55 +0900

    -feat json 응답을 처리하기 위한 userdetailsservice

---
+++ src/main/java/com/rchat/randomChat/security/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.rchat.randomChat.security.service;
+
+import com.rchat.randomChat.global.repository.UserRepository;
+import com.rchat.randomChat.global.repository.entity.UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo userInfo = userRepository.findByEmail(username).orElseThrow();
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(userInfo.getEmail())
+                .password(userInfo.getPw())
+                .roles(userInfo.getRole().toString())
+                .build();
+    }
+}
