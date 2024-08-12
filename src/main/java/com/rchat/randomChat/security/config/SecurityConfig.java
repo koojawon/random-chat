2d182f2ecf1226907cd2109816c04d886d8a5093
koojawon <jawonkoo@naver.com>
2024-08-12 16:34:15 +0900

    -feat spring security 설정 추가

---
+++ src/main/java/com/rchat/randomChat/security/config/SecurityConfig.java
@@ -0,0 +1,109 @@
+package com.rchat.randomChat.security.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rchat.randomChat.security.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.rchat.randomChat.security.filter.JwtAuthenticationProcessingFilter;
+import com.rchat.randomChat.security.filter.RefreshRequestProcessingFilter;
+import com.rchat.randomChat.security.handler.CustomAccessDeniedHandler;
+import com.rchat.randomChat.security.handler.CustomLogoutSuccessHandler;
+import com.rchat.randomChat.security.handler.LoginFailureHandler;
+import com.rchat.randomChat.security.handler.LoginSuccessHandler;
+import com.rchat.randomChat.security.service.CustomUserDetailsService;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter;
+    private final RefreshRequestProcessingFilter refreshRequestProcessingFilter;
+    private final ObjectMapper objectMapper;
+    private final LoginFailureHandler loginFailureHandler;
+    private final LoginSuccessHandler loginSuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/", "/api/signup").permitAll()
+                                .requestMatchers("/admin").hasRole("admin")
+                                .anyRequest().denyAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout(c -> c.logoutUrl("/api/logout")
+                        .clearAuthentication(true)
+                        .logoutSuccessHandler(customLogoutSuccessHandler))
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterAfter(jsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationProcessingFilter,
+                        JsonUsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(refreshRequestProcessingFilter, JwtAuthenticationProcessingFilter.class)
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.accessDeniedHandler(new CustomAccessDeniedHandler()))
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
+                        .configurationSource(corsConfigurationSource()))
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        configuration.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::toString).toList());
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        ProviderManager manager = new ProviderManager(daoAuthenticationProvider);
+        manager.setEraseCredentialsAfterAuthentication(false);
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter =
+                new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler);
+        jsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler);
+        return jsonUsernamePasswordAuthenticationFilter;
+    }
+}
+
