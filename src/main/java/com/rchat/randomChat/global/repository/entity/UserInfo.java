e0968893f64ebfac160afd2a77638f9ddab39127
koojawon <jawonkoo@naver.com>
2024-08-12 16:33:40 +0900

    -feat 사용자 엔티티 추가

---
+++ src/main/java/com/rchat/randomChat/global/repository/entity/UserInfo.java
@@ -0,0 +1,36 @@
+package com.rchat.randomChat.global.repository.entity;
+
+import com.rchat.randomChat.security.Roles;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column
+    private Roles role;
+}
