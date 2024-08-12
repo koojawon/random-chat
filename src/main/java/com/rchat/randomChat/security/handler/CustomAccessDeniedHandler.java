dec4f9e700cde77605aa8e4e9868fcc49f174f10
koojawon <jawonkoo@naver.com>
2024-08-12 20:03:36 +0900

    -refactor objectmapper 의존성 주입

--- src/main/java/com/rchat/randomChat/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/rchat/randomChat/security/handler/CustomAccessDeniedHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -13,9 +14,10 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
