c86635f752a73bac171038db3620877957a7fab1
koojawon <jawonkoo@naver.com>
2024-08-12 16:39:18 +0900

    -feat 인가 실패시 핸들러 추가

---
+++ src/main/java/com/rchat/randomChat/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package com.rchat.randomChat.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rchat.randomChat.global.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ApiResponse<?> apiResponse = ApiResponse.createError(accessDeniedException.getMessage());
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+    }
+}
